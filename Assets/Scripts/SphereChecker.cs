e4edd43536618cb30b4d78e17406c203b3c7d145
maikonfarias <maikonfarias@MacBook-Pro-de-Maikon.local>
2016-01-30 11:02:07 -0700

    Sphere Checker update

---
+++ Assets/Scripts/SphereChecker.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereChecker : MonoBehaviour
+{
+  public GameObject checkedObject;
+
+  public GnomeColorRequirement requireColor;
+  public enum GnomeColorRequirement {
+    Any,
+    Red,
+    Yellow
+  }
+
+  int numberOfCollidingGnomes = 0;
+
+
+  void Update()
+  {
+    if(numberOfCollidingGnomes > 0)
+    {
+      checkedObject.SetActive(true);
+    } else {
+      checkedObject.SetActive(false);
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    var component = other.gameObject.GetComponent<GnomeController>();
+    if (component != null)
+    {
+      if((requireColor == GnomeColorRequirement.Red && component.MyColor == GnomeController.GnomeColor.Red) ||
+        (requireColor == GnomeColorRequirement.Yellow && component.MyColor == GnomeController.GnomeColor.Yellow) ||
+        requireColor == GnomeColorRequirement.Any){
+        numberOfCollidingGnomes++;
+      }
+    }
+  }
+
+  void OnTriggerExit2D(Collider2D other)
+  {
+    var component = other.gameObject.GetComponent<GnomeController>();
+    if (component != null)
+    {
+      if((requireColor == GnomeColorRequirement.Red && component.MyColor == GnomeController.GnomeColor.Red) ||
+        (requireColor == GnomeColorRequirement.Yellow && component.MyColor == GnomeController.GnomeColor.Yellow) ||
+        requireColor == GnomeColorRequirement.Any){
+        numberOfCollidingGnomes--;
+      }
+    }
+  }
+}
