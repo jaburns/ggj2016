c74b861d6fd1c165dc5ffbe8232b8d90665322ea
Jeremy Burns <j@jaburns.net>
2016-01-29 20:23:36 -0700

    Added controls for the different gnomes

--- Assets/Scripts/GnomeController.cs
+++ Assets/Scripts/GnomeController.cs
@@ -2,6 +2,12 @@
 
 public class GnomeController : MonoBehaviour
 {
+    public float MaxRunSpeed;
+    public float JumpImpulse;
+    public float RunForce;
+    public float TurnAroundMultiplier;
+    public float DecelMultiplier;
+
     public enum GnomeColor {
         Red,
         Yellow
@@ -10,6 +16,7 @@ public class GnomeController : MonoBehaviour
     public GnomeColor MyColor;
 
     Rigidbody2D _rb;
+    GnomeController _onHead;
 
     void Awake()
     {
@@ -19,6 +26,43 @@ public class GnomeController : MonoBehaviour
     void FixedUpdate()
     {
         var inputs = InputGrabber.Instance.GetInputsForColor(MyColor);
-        _rb.AddForce(10* Vector2.right * inputs.WalkAxis);
+
+        var runForce = RunForce * Vector2.right * inputs.WalkAxis;
+        if (runForce.x * _rb.velocity.x < 0) runForce *= TurnAroundMultiplier;
+        _rb.AddForce(runForce);
+
+        if (_onHead) {
+            var hb = _onHead.GetComponent<Rigidbody2D>();
+            hb.AddForce(runForce / _rb.mass * hb.mass);
+        }
+
+        if (inputs.JumpButton.JustPressed) {
+            _rb.AddForce(JumpImpulse * Vector2.up, ForceMode2D.Impulse);
+        }
+
+        if (Mathf.Abs(_rb.velocity.x) > MaxRunSpeed) {
+            _rb.velocity = _rb.velocity.WithX(MaxRunSpeed * Mathf.Sign(_rb.velocity.x));
+        }
+
+        if (Mathf.Abs(inputs.WalkAxis) < .1f) {
+            _rb.velocity = _rb.velocity.WithX(_rb.velocity.x * DecelMultiplier);
+        }
+
+        _onHead = null;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) { OnCollisionStay2D(col); }
+    void OnCollisionStay2D(Collision2D col)
+    {
+        var colGnome = col.collider.gameObject.GetComponent<GnomeController>();
+        if (colGnome == null) return;
+
+        bool isTop = false;
+        foreach (var contact in col.contacts) {
+            if (contact.normal.y < -0.8f) isTop = true;
+        }
+        if (!isTop) return;
+
+        _onHead = colGnome;
     }
 }
