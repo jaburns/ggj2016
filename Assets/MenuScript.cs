c36cbd1e453d086a21ad0c85bb792c136b71ae49
maikonfarias <maikonfarias@MacBook-Pro-de-Maikon.local>
2016-01-29 20:39:40 -0700

    Main Menu Scene

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+  public GameObject MainMenuScreen;
+  public GameObject OptionsMenuScreen;
+
+  public void StartButtonClick()
+  {
+    SceneManager.LoadScene(1);
+  }
+
+  public void OptionsButtonClick()
+  {
+    MainMenuScreen.SetActive(false);
+    OptionsMenuScreen.SetActive(true);
+  }
+
+  public void QuitButtonClick() 
+  {
+    Application.Quit();
+  }
+
+  public void OptionsBackButtonClick()
+  {
+    MainMenuScreen.SetActive(true);
+    OptionsMenuScreen.SetActive(false);
+  }
+}
