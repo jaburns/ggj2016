2bd6afc2ef9cb1c69fca948645ed724fbda82a2f
Jeremy Burns <j@jaburns.net>
2016-01-29 18:58:59 -0700

    Added some script utils, unlit materials, and gnome cubes

---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    static T s_instance;
+    static bool s_quitting;
+
+    static public T Instance
+    {
+        get {
+            if (s_quitting) return null;
+            if (s_instance != null) return s_instance;
+
+            s_instance = FindObjectOfType<T>();
+
+            if (s_instance == null) {
+                var go = new GameObject();
+                s_instance = go.AddComponent<T>();
+                go.name = "[Singleton] " + typeof(T);
+            }
+
+            return s_instance;
+        }
+    }
+
+    public void OnApplicationQuit()
+    {
+        s_quitting = true;
+    }
+}
