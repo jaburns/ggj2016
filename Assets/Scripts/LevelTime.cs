75e9a67bb7f0ceac84d74be71d4834d3ac6d27c6
Jeremy Burns <j@jaburns.net>
2016-01-31 13:25:23 -0700

    LevelTime moves fader to center

--- Assets/Scripts/LevelTime.cs
+++ Assets/Scripts/LevelTime.cs
@@ -3,4 +3,10 @@
 public class LevelTime : MonoBehaviour
 {
     public float LevelLength = 10f;
+
+    void Awake()
+    {
+        var fader = GameObject.Find("Faderz");
+        fader.transform.position = fader.transform.position.WithXY(0, 0);
+    }
 }
