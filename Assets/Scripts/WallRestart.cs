a1446c8eb66bf0acffe3629a1a06fec053d9cab4
maikonfarias <maikonfarias@MacBook-Pro-de-Maikon.local>
2016-01-31 15:50:10 -0700

    Walk off to restart

---
+++ Assets/Scripts/WallRestart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallRestart : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        MusicPlayer.Instance.Stop();
+        SceneLoader.ReloadLevel();
+    }
+}
